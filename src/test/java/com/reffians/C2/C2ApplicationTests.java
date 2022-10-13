886847591bac4090d9cbd97648bc11f95fe96e07
Adam Storek <storek.adam@gmail.com>
2022-10-12 23:47:51 -0400

    Added hello world source.

---
+++ src/test/java/com/reffians/C2/C2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reffians.C2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
