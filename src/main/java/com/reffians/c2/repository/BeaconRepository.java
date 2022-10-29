77fe480006bf1802e01eb9567538d3565c452d39
Adam Storek <storek.adam@gmail.com>
2022-10-29 19:31:14 -0400

    Removed unused imports in beacon repository.

--- src/main/java/com/reffians/c2/repository/BeaconRepository.java
+++ src/main/java/com/reffians/c2/repository/BeaconRepository.java
@@ -1,11 +1,7 @@
 package com.reffians.c2.repository;
 
 import com.reffians.c2.model.Beacon;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 /** Beacon Repository to query beacons table. */
 public interface BeaconRepository extends CrudRepository<Beacon, Integer> {
