9b3e3e0250011d947f548da2cca069ee8f21b6eb
Adam Storek <storek.adam@gmail.com>
2022-11-26 19:08:30 -0500

    Moved current security configuration under security password configuration.

--- src/main/java/com/reffians/c2/configuration/security/PasswordConfiguration.java
+++ src/main/java/com/reffians/c2/configuration/security/PasswordConfiguration.java
@@ -1,12 +1,12 @@
-package com.reffians.c2.configuration;
+package com.reffians.c2.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-/** Aplication Security Configuration. */
+/** Password Configuration. */
 @Configuration
-public class ApplicationSecurityConfiguration {
+public class PasswordConfiguration {
   @Bean
   public BCryptPasswordEncoder bcryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
