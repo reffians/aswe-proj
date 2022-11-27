e3f88e3c2408d96562fef29b796b61f02f809284
Adam Storek <storek.adam@gmail.com>
2022-11-26 19:02:38 -0500

    User model now implements UserDetails.

--- src/main/java/com/reffians/c2/model/User.java
+++ src/main/java/com/reffians/c2/model/User.java
@@ -1,18 +1,21 @@
 package com.reffians.c2.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /** User table. **/
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
   @Id
   @Getter
   @JsonProperty("username")
@@ -25,5 +28,35 @@ public class User {
   public User(String username, String rawPassword, PasswordEncoder passwordEncoder) {
     this.username = username;
     this.encodedPassword = passwordEncoder.encode(rawPassword);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getPassword() {
+    return getEncodedPassword();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 }
