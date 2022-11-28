f23d00bba84de3e3867d67d04197d54d76b174c3
Adam Storek <storek.adam@gmail.com>
2022-11-28 16:34:09 -0500

    Added validation to UserRequest.

--- src/main/java/com/reffians/c2/dto/UserRequest.java
+++ src/main/java/com/reffians/c2/dto/UserRequest.java
@@ -1,14 +1,19 @@
 package com.reffians.c2.dto;
 
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.Generated;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.annotation.Validated;
 
 /** User Request DTO. */
 @Generated
 @Data
+@Validated
 public class UserRequest {
+  @NotEmpty
   private String username;
+  @NotEmpty
   private String password;
 
   public UsernamePasswordAuthenticationToken getAuthenticationToken() {
